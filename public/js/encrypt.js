366981ca423a9fc6431af2b31b2aa624a86261ab
gercsz <gerberto22@gmail.com>
2022-09-01 16:55:25 -0300

    Finalizado funcionalidades

--- public/js/encrypt.js
+++ public/js/encrypt.js
@@ -1,44 +1,31 @@
 
-const initialText = document.getElementById("initial-text")
-const finalText = document.getElementById("final-text")
 
-function btnEncryptText(){
-    const encryptedText = encryptText(initialText.value);
-    finalText.value = encryptedText;
-    document.getElementById("final-text-elements").style.visibility = "hidden";
-}
-
-function btnDecrypt(){
-    const decryptedText = decryptText(initialText.value);
-    finalText.value = decryptedText;
-    document.getElementById("final-text-elements").style.visibility = "hidden";
-}
 
 function encryptText(text) {
 
-    
+
     let modifiedText = "";
 
-    
+
     for (let index = 0; index < text.length; index++) {
 
         let letter = text[index];
 
         switch (letter) {
-            case 'a': modifiedText+='ai';
-                break;                
-            case 'e': modifiedText+='enter';
+            case 'a': modifiedText += 'ai';
                 break;
-            case 'i': modifiedText+='imes';
+            case 'e': modifiedText += 'enter';
                 break;
-            case 'o': modifiedText+='ober';
+            case 'i': modifiedText += 'imes';
                 break;
-            case 'u': modifiedText+='ufat';
+            case 'o': modifiedText += 'ober';
                 break;
-            default: modifiedText+=letter;
+            case 'u': modifiedText += 'ufat';
+                break;
+            default: modifiedText += letter;
                 break;
         }
-        
+
     }
 
     return modifiedText;
@@ -47,51 +34,93 @@ function encryptText(text) {
 function decryptText(text) {
 
     let modifiedText = "";
-    let auxText = ""
+
 
     for (let index = 0; index < text.length; index++) {
 
         let letter = text[index];
-        // if (index == 0){
-        //     auxText = text.substring(index, index + 3);}
 
         switch (letter) {
             case 'a':
-                modifiedText+=letter;
+                modifiedText += letter;
                 if (text.substring(index, index + 2) == 'ai') {
                     index += 1;
                 }
                 break;
             case 'e':
-                modifiedText+=letter;
+                modifiedText += letter;
                 if (text.substring(index, index + 5) == 'enter') {
                     index += 4;
                 }
                 break;
             case 'i':
-                modifiedText+=letter;
+                modifiedText += letter;
                 if (text.substring(index, index + 4) == 'imes') {
                     index += 3;
                 }
                 break;
             case 'o':
-                modifiedText+=letter;
+                modifiedText += letter;
                 if (text.substring(index, index + 4) == 'ober') {
                     index += 3;
-                } 
+                }
                 break;
             case 'u':
-                modifiedText+=letter;
+                modifiedText += letter;
                 if (text.substring(index, index + 4) == 'ufat') {
                     index += 3;
-                } 
+                }
                 break;
-
-            default: modifiedText+=letter;
+            default: modifiedText += letter;
                 break;
         }
-
     }
+    return modifiedText;
+}
 
-    return modifiedText;  
+
+
+const initialText = document.getElementById("initial-text")
+const finalText = document.getElementById("final-text")
+
+function btnEncryptText() {
+    const encryptedText = encryptText(initialText.value);
+    finalText.value = encryptedText;
+    document.getElementById("final-text-elements").style.visibility = "hidden";
+}
+
+function btnDecrypt() {
+    const decryptedText = decryptText(initialText.value);
+    finalText.value = decryptedText;
+    document.getElementById("final-text-elements").style.visibility = "hidden";
+}
+
+function btnCopy() {
+
+    const content = document.getElementById('final-text').value;
+
+    navigator.clipboard.writeText(content)
+    .then(() => {
+        console.log('Text copied to clipboard');
+    })
+    .catch(err => {
+        console.error('Error in copying text: ', err);
+    });
+    alert('¡copiado!')
+
+}
+
+
+document.getElementById("initial-text").addEventListener("keypress", verificar);
+function verificar(e) {
+
+    // comprovamos con una expresion regular que el caracter pulsado sea
+    // una letra, numero o un espacio
+    
+    if (e.key.match(/[a-z0-9ñç\s]/i) === null) {
+
+        // Si la tecla pulsada no es la correcta, eliminado la pulsación
+        e.preventDefault();
+    }
+    
 }
